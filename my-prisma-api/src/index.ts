9f7ca77f3e39cf3ac78d78e4ee5fb01e62ba14b7
Wisarut_folk <wisrutyaemprayur@gmail.com>
2026-02-23 16:33:27 +0700

    Update index.ts

--- my-prisma-api/src/index.ts
+++ my-prisma-api/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { prisma } from '../lib/prisma';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -7,6 +8,45 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Prisma API!');
+});
+
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: { name, email },
+    });
+    res.status(201).json(user);
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+})
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.update({
+      where: { Userid: id },
+      data: { name, email },
+    });
+    res.json(user);
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Failed to update user' });
+  } 
 });
 
 app.listen(port, () => {
